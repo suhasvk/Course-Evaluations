2807cec041847aa1ffa06b6e29eabd38116cb49e
Suhas Vijaykumar <suhasv@Suhass-MacBook-Pro.local>
2015-04-28 02:09:22 -0400

    added nav-bar interactions, search form modal

--- Site/Client/Site.js
+++ Site/Client/Site.js
@@ -1,1 +1,17 @@
 // Site.js
+$(function(){
+	// $('.modal').modal();
+	$('#navbar').find('li').each(function(){
+		$(this).click(function(evt){
+			if (!$(this).hasClass('active')){
+				$('.panel').hide();
+				var panelIdSelector = $(this).children('a').attr('href') + '-panel';
+				$(panelIdSelector).show();
+				$('.collapse').collapse('hide');
+				if (panelIdSelector === '#results-panel'){
+					$('#search-modal').modal('show');
+				}
+			}
+		});
+	});
+});
